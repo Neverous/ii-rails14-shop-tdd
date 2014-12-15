3fa7da74066fd9bf6a3e4c6129a771eee8836d87
Maciej Szeptuch (Neverous) <neverous@neverous.info>
2014-12-15 01:53:38 +0100

    models specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "category requires name" do
+        category = Category.new()
+        expect(category.valid?).to eq(false)
+    end
+
+    it "valid category" do
+        category = Category.new(name: 'test')
+        expect(category.valid?).to eq(true)
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "product requires name, price, category" do
+        product = Product.new()
+        expect(product.valid?).to eq(false)
+
+        product = Product.new(name: 'test')
+        expect(product.valid?).to eq(false)
+
+        product = Product.new(name: 'test', price: 3.14)
+        expect(product.valid?).to eq(false)
+
+        product = Product.new(name: 'test', price: 3.14, category_id: 9)
+        expect(product.valid?).to eq(false)
+    end
+
+    it "valid product" do
+        category = Category.create(name: 'test')
+        product = Product.new(name: 'test', price: 3.14, category: category)
+        expect(product.valid?).to eq(true)
+    end
 end
